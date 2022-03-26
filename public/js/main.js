c2521f94ef5e3004776021b4adc2361136a58af4
Adam Rowe <admrwe@gmail.com>
2022-03-26 15:51:53 -0600

    chore: Ported site to new repo.

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+document.body.className += ' fade-out';
+
+function preloader() {
+    document.body.classList.remove("fade-out");
+}
+
+window.onload = preloader;
+
+
+(function () {
+    var ll = new LazyLoad({
+        elements_selector: ".lazy"
+    });
+})();
